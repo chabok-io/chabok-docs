7e290e90122adc3c2a78d66fe613609905bfd982
J.Azimi <jalalazimi@gmail.com>
2018-01-22 16:07:36 +0330

    handle anchor on headers

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -1,101 +1,84 @@
-$(document).ready(function () {
-
-    var windowHeight = 0;
-    var windowWidth = 0;
-    window.direction = 'left';
-    var citysvg = 0;
-
-    function chnageSizeOfWindwo() {
-
-        if ($(window).height() > 600) {
-            windowHeight = $(window).height()
-        } else {
-            windowHeight = 600
-        }
-        if ($(window).width() > 1200) {
-            windowWidth = $(window).width()
-        } else {
-            windowWidth = 1200;
-        }
-        $('.border').css({
-            borderRight: windowWidth + 'px solid #fff'
-        })
-        var cityheight = windowWidth * .25
-        $('.citysvg').css({
-            height: cityheight
-        })
-        $('.background > .circle').css({
-            bottom: 165 + ((cityheight - 300 ) / 2)
-        });
-        $('.background > .threeg').css({
-            bottom: 165 + ((cityheight - 300 ) / 2)
-        });
-        $('.background > .bubble').css({
-            bottom: 165 + ((cityheight - 300 ) / 2)
-        });
-        $('.background > .antena').css({
-            bottom: 165 + ((cityheight - 300 ) / 2)
-        });
-
-        boatbottom = 20 + ((cityheight - 300 ) / 15);
-        $('.boatholder').css({
-            bottom: boatbottom
-        });
-        $('.car').css({
-            left: windowWidth / 4.8
-        });
-
-
-        $('.fullSlide').css({
-            width: windowWidth,
-            height: windowHeight
-        })
-        $('.halfSlide').css({
-            width: windowWidth,
-            height: windowHeight / 2
-        })
-        $('.halfSlidemin').css({
-            width: windowWidth,
-            minHeight: windowHeight / 2
-        })
-    }
-
-    function gotoSlide2() {
-        $('.gotoSlide2').click(function () {
-            $('html,body').animate({scrollTop: windowHeight}, '1000', 'linear')
-        })
-    }
-
-    function khabarname() {
-        $('.khabrnamebutt').click(function () {
-            var userEmail = $('#khabrnameEmail').val();
+function khabarname() {
+    $('body')
+        .on('click', '.khabrnamebutt', function () {
+            const userEmail = $('#khabrnameEmail').val();
             if (!userEmail || userEmail == '' || !userEmail.match(/.+@.+\..+/)) {
-                return alert('Ø¢Ø¯Ø±Ø³ Ø§ÛŒÙ…ÛŒÙ„ Ø´Ù…Ø§ Ø¯Ø±Ø³Øª ÙˆØ§Ø±Ø¯ Ù†Ø´Ø¯Ù‡ Ø§Ø³Øª');
+                return alert('آدرس ایمیل شما درست وارد نشده است');
             }
-            $.ajax({
-                url: 'https://push.adpdigital.com/api/email/subscribe',
-                type: 'POST',
-                data: {
-                    email: userEmail,
-                    list: 'users',
-                    appId: 'chabok-demo'
-                }
-            })
+            $
+                .ajax({
+                    url: 'https://push.adpdigital.com/api/email/subscribe',
+                    type: 'POST',
+                    data: {
+                        email: userEmail,
+                        list: 'users',
+                        appId: 'chabok-demo'
+                    }
+                })
                 .done(function (request) {
-                    alert('Ø¢Ø¯Ø±Ø³ Ø§ÛŒÙ…ÛŒÙ„ Ø´Ù…Ø§ Ø¨Ø§ Ù…ÙˆÙÙ‚ÛŒØª Ø¨Ù‡ Ø®Ø¨Ø±Ù†Ø§Ù…Ù‡ Ú†Ø§Ø¨Ú© Ø§ÙØ²ÙˆØ¯Ù‡ Ø´Ø¯');
+                    alert('آدرس ایمیل شما با موفقیت به خبرنامه چابک افزوده شد');
                 })
                 .fail(function (err) {
-                    alert('Ø®Ø·Ø§ Ø¯Ø± Ø§Ø±ØªØ¨Ø§Ø· Ø¨Ø§ Ø³Ø±ÙˆØ±ØŒ Ù„Ø·ÙÙ† Ø¨Ø¹Ø¯Ù† Ø¯ÙˆØ¨Ø§Ø±Ù‡ Ø§Ù…ØªØ­Ø§Ù† Ú©Ù†ÛŒØ¯');
+                    alert('خطا در ارتباط با سرور، لطفن بعدن دوباره امتحان کنید');
                     $('.loginError').html(JSON.parse(err.responseText).error.message)
                 })
         })
-    }
+}
 
-    $(window).resize(function () {
-        chnageSizeOfWindwo()
+
+function sideMenu() {
+    var $body = $('body'),
+        $html = $('html'),
+        $sidebar = $('#sidebar');
+
+    $body.on('click', '.overlayModal', function () {
+        var $overlayModal = $('.overlayModal');
+
+        if ($overlayModal.length) {
+            $overlayModal.remove();
+        }
+        $sidebar.removeClass('drawer-menu-active');
+        $html.removeClass('noScroll');
+
     });
 
+    $('#sideMenuHandler').on('click', function () {
+        var $overlayModal = $('.overlayModal');
+        $sidebar.addClass('drawer-menu-active');
+        $html.addClass('noScroll');
+        if ($overlayModal.length) {
+            $overlayModal.remove();
+        }
+        $body.append('<div class="overlayModal"></div>')
+    })
+}
+
+$(document).ready(function () {
     khabarname();
-    chnageSizeOfWindwo();
-    gotoSlide2();
+    sideMenu();
+
+    var elem = window.location.hash ? $('#' + decodeURIComponent(window.location.hash.replace('#', ''))) : ''
+    var HEADER_HEIGHT = 75;
+
+    $('.h2,h3,h4,h5,h6').filter('[id]').each(function () {
+        $(this).html('<a href="#' + $(this).attr('id') + '">' + $(this).text() + '</a>');
+    });
+
+    if (elem.length) {
+        $('html, body').animate({
+            scrollTop: elem.offset().top - HEADER_HEIGHT
+        }, 1000);
+    }
+
+    $('a[href^="#"]').click(function () {
+        var href = $.attr(this, 'href');
+
+        $('html, body').animate({
+            scrollTop: $(href).offset().top - HEADER_HEIGHT
+        }, 500);
+        window.location.hash = href;
+
+        return false;
+    });
+
 });
