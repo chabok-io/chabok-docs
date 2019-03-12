5acf2c76aeaa0c8d8a44d21c41c0f1145dd958db
marjan <marjan.jangholi@yahoo.com>
2019-03-12 14:24:36 +0330

    add style to first child

--- _assets/javascript/app.js
+++ _assets/javascript/app.js
@@ -16,9 +16,9 @@
         var height = $(window).height() - (visibleBottom - visibleTop)
 
         if ((visibleBottom - visibleTop) > 0){
-            $('.toc-list').css("height", height)
+            $('#toc ul:first-child').css("height", height)
         } else {
-            $('.toc-list').css("height", 'auto')
+            $('#toc ul:first-child').first().css("height", 'auto')
         }
     }
     $(window).on('scroll resize', getVisiblePartOfFooter);
