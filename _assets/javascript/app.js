6c2c314643b622d340fb16f60e108069d0973947
marjan <marjan.jangholi@yahoo.com>
2019-03-12 13:25:25 +0330

    add overflow to class

--- _assets/javascript/app.js
+++ _assets/javascript/app.js
@@ -16,9 +16,9 @@
         var height = $(window).height() - (visibleBottom - visibleTop)
 
         if ((visibleBottom - visibleTop) > 0){
-            $('#toc').css("height", height)
+            $('.toc-list').css("height", height)
         } else {
-            $('#toc').css("height", 'auto')
+            $('.toc-list').css("height", 'auto')
         }
     }
     $(window).on('scroll resize', getVisiblePartOfFooter);
