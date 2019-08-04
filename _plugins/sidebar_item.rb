2d7e414a343388f33ea96044bc42cfeacb684245
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2019-08-05 01:02:09 +0430

    Change subitem font size to 12

--- _plugins/sidebar_item.rb
+++ _plugins/sidebar_item.rb
@@ -73,7 +73,7 @@ module Jekyll
       if subItems != nil && pageID == itemID
         result = "<ul><a href=\"#{href}\"#{className}>#{item["title"]}</a>"
         subItems.each {|curItem|
-          result += "<li><a href=\"#{pageID}.html##{curItem["href"]}\">#{curItem["title"]}</a></li>"
+          result += "<li><a style='font-size: 12px;' href=\"#{pageID}.html##{curItem["href"]}\">#{curItem["title"]}</a></li>"
         }
         result += "</ul>"
       else
