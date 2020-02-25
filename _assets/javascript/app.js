b63ceef4f34d205b510cb184018b5cef219a7426
Hussein <Hussein.Juybari+github@gmail.com>
2020-02-25 13:47:37 +0330

    Remove unused code

--- _assets/javascript/app.js
+++ _assets/javascript/app.js
@@ -387,8 +387,6 @@ $(document).ready(function () {
   var HEADER_HEIGHT = 75;
 
   $('h2,h3,h4,h5,h6').filter('[id]').each(function () {
-    $(this).html('<a href="#' + $(this).attr('id') + '">' + $(this).text() + '</a>');
-
     const innerText = $(this).text() || ''
     const headerText = innerText.split('|')
 
