b27c4fe9bec5e8ff0023756d6e2efabde25d1d39
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2020-12-30 12:07:16 +0330

    Update build-and-deploy.sh script

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -37,15 +37,15 @@ jekyll build &&
 echo '👍 THE SITE IS BUILT—PUSHING IT BACK TO GITHUB-PAGES' && 
 
 cd /srv/jekyll/_site && 
-remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" &&
+remote_repo="https://x-access-token:${env.GITHUB_TOKEN}@github.com/${env.GITHUB_REPOSITORY}.git" &&
 remote_branch="gh-pages" && 
 
 echo remote_branch $remote_branch &&
 echo remote_repo $remote_repo &&
 
 git init && 
-git config user.name --global "${GITHUB_ACTOR}" && 
-git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com" &&
+git config user.name --global "${env.GITHUB_ACTOR}" &&
+git config --global user.email "${env.GITHUB_ACTOR}@users.noreply.github.com" &&
 git add . && 
 
 echo -n 'Files to Commit:' && ls -l | wc -l && 
