dbf627b98852cec529e99a006b8b4c95305888cc
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2021-01-24 15:23:41 +0330

    Update sidebar_item.rb

--- _plugins/sidebar_item.rb
+++ _plugins/sidebar_item.rb
@@ -1,66 +1,66 @@
 module Jekyll
   module SidebarItemFilter
-    def docs_sidebar_link(item, baseurl = '---')
+    def docs_sidebar_link(item, baseurl = '')
       return sidebar_helper(item, 'docs', baseurl)
     end
 
-    def ios_sidebar_link(item, baseurl = '---')
+    def ios_sidebar_link(item, baseurl = '')
       return sidebar_helper(item, 'ios', baseurl)
     end
 
-    def android_sidebar_link(item, baseurl = '---')
+    def android_sidebar_link(item, baseurl = '')
       return sidebar_helper(item, 'android', baseurl)
     end
 
-    def glossary_sidebar_link(item, baseurl = '---')
+    def glossary_sidebar_link(item, baseurl = '')
       return sidebar_helper(item, 'glossary', baseurl)
     end
 
-    def unity_sidebar_link(item, baseurl = '---')
+    def unity_sidebar_link(item, baseurl = '')
         return sidebar_helper(item, 'unity', baseurl)
     end
 
-    def flutter_sidebar_link(item, baseurl = '---')
+    def flutter_sidebar_link(item, baseurl = '')
         return sidebar_helper(item, 'flutter', baseurl)
     end
 
-    def cordova_sidebar_link(item, baseurl = '---')
+    def cordova_sidebar_link(item, baseurl = '')
         return sidebar_helper(item, 'cordova', baseurl)
     end
 
-    def faq_sidebar_link(item, baseurl = '---')
+    def faq_sidebar_link(item, baseurl = '')
         return sidebar_helper(item, 'faq', baseurl)
     end
 
-    def windows_sidebar_link(item, baseurl = '---')
+    def windows_sidebar_link(item, baseurl = '')
       return sidebar_helper(item, 'windows', baseurl)
     end
 
-    def guides_sidebar_link(item, baseurl = '---')
+    def guides_sidebar_link(item, baseurl = '')
           return sidebar_helper(item, 'guides', baseurl)
     end
 
-    def mobile_usecases_sidebar_link(item, baseurl = '---')
+    def mobile_usecases_sidebar_link(item, baseurl = '')
           return sidebar_helper(item, 'mobile-usecases', baseurl)
     end
 
-    def panel_sidebar_link(item, baseurl = '---')
+    def panel_sidebar_link(item, baseurl = '')
         return sidebar_helper(item, 'panel', baseurl)
     end
 
-    def javascript_sidebar_link(item, baseurl = '---')
+    def javascript_sidebar_link(item, baseurl = '')
           return sidebar_helper(item, 'javascript', baseurl)
     end
 
-    def react_native_sidebar_link(item, baseurl = '---')
+    def react_native_sidebar_link(item, baseurl = '')
           return sidebar_helper(item, 'react-native', baseurl)
     end
 
-    def react_native_bridge_sidebar_link(item, baseurl = '---')
+    def react_native_bridge_sidebar_link(item, baseurl = '')
           return sidebar_helper(item, 'react-native-bridge', baseurl)
     end
 
-    def rest_api_sidebar_link(item, baseurl = '---')
+    def rest_api_sidebar_link(item, baseurl = '')
           return sidebar_helper(item, 'rest-api', baseurl)
     end
 
